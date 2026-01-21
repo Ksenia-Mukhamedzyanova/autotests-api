c9d78382c2a0c62f8820c0ac07b6ecfce91cf5d5
Ksenia Mukhamedzyanova <ksenia.mukhamedzyanova@mail.ru>
2026-01-21 21:59:23 +0500

    Рефакторинг и подготовка к написанию API автотестов

--- tests/authentication/test_authentication.py
+++ tests/authentication/test_authentication.py
@@ -28,3 +28,22 @@ def test_login(
 
     validate_json_schema(response.json(), response_data.model_json_schema())
 
+@pytest.mark.regression
+@pytest.mark.authentication
+class TestAuthentication:
+    def test_login(
+            self,
+            function_user: UserFixture,  # Используем фикстуру для создания пользователя
+            authentication_client: AuthenticationClient
+    ):
+        # Запрос на логин (login_request -> request)
+        request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+        # Выполняем логин (login_response -> response)
+        response = authentication_client.login_api(request)
+        # Валидация ответа (login_response_data -> response_data)
+        response_data = LoginResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_login_response(response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
